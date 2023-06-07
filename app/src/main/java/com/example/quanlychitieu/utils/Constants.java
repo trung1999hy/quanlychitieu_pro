3e8542b29bd08ea1dbe1880caa21e36bc6751e6f
Trần Nam <65379025+tranam1312@users.noreply.github.com>
2023-06-07 09:15:03 +0700

    change key

--- app/src/main/java/com/example/quanlychitieu/utils/Constants.java
+++ app/src/main/java/com/example/quanlychitieu/utils/Constants.java
@@ -1,13 +1,11 @@
 package com.example.quanlychitieu.utils;
 
 public class Constants {
-
-    public static final String KEY_COIN = "KEY_COIN"; // 5 coin
-
-    public static final String KEY_10_COIN = "key_100"; // 100 coin
-    public static final String KEY_20_COIN = "key_150"; // 150 coin
-    public static final String KEY_50_COIN = "key_300"; // 300 coin
-    public static final String KEY_100_COIN = "key_500"; // 500 coin
-    public static final String KEY_150_COIN = "key_700"; // 700 coin
-    public static final String KEY_200_COIN = "key_999"; // 999 coin
+    public static final String KEY_COIN = "key_7"; // 5 coin
+    public static final String KEY_10_COIN = "key_8"; // 100 coin
+    public static final String KEY_20_COIN = "key_9"; // 150 coin
+    public static final String KEY_50_COIN = "key_10"; // 300 coin
+    public static final String KEY_100_COIN = "key_11"; // 500 coin
+    public static final String KEY_150_COIN = "key_12"; // 700 coin
+    public static final String KEY_200_COIN = "key_13"; // 999 coin/ 999 coin
 }
